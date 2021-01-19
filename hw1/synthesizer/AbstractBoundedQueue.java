c84904a3058270e91cead9368533701885b17d78
Seohui Choe <chelsea.choe@gmail.com>
2021-01-18 20:59:37 -0500

    Start hw1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,12 @@
+package synthesizer;
+
+abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
+    protected int fillCount;
+    protected int capacity;
+    public int capacity() {
+        return capacity;
+    }
+    public int fillCount() {
+        return fillCount;
+    }
+}
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        this.fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -38,7 +43,8 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        return null;
     }
 
     /**
@@ -46,6 +52,7 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        return null;
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
