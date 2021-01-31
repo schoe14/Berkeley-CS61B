c20b1efbf2cd838de1297266348ebf7c7a2c962f
Seohui Choe <chelsea.choe@gmail.com>
2021-01-30 20:33:54 -0500

    Need to figure out how to open the site

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,34 +3,60 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int size;
-    private WeightedQuickUnionUF grid;
+    private boolean[][] grid;
+    private int len;
+    private int top = 0;
+    private int bottom;
+    private WeightedQuickUnionUF uf;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException();
-        } else {
-            size = N;
-            grid = new WeightedQuickUnionUF(N);
+        }
+        len = N;
+        grid = new boolean[N][N];
+        bottom = N * N + 1;
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        int i = 1;
+        while (i <= N) {
+            uf.union(top, i);
+            uf.union(bottom, bottom - i);
+            i++;
         }
     }
 
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (!isOpen(row, col)) {
-
+        if (row < 0 || row > len - 1 || col < 0 || col > len - 1) {
+            throw new IndexOutOfBoundsException();
         }
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+        }
+        // need to work on it
+    }
+
+    // 2D array to 1D
+    public int xyTo1D(int r, int c) {
+        return len * r + c;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        if (row < 0 || row > len - 1 || col < 0 || col > len - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return grid[row][col];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return false;
+        if (row < 0 || row > len - 1 || col < 0 || col > len - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        int to1D = xyTo1D(row, col) + 1;
+        return uf.find(to1D) == top;
     }
 
     // number of open sites
@@ -45,6 +71,8 @@ public class Percolation {
 
     // use for unit testing (not required)
     public static void main(String[] args) {
-
+        Percolation p = new Percolation(3);
+        System.out.println(p.isOpen(0,2));
+        System.out.println(p.isFull(1,1));
     }
 }
